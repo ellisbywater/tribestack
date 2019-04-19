d22871ec76ed3a21083a94beb49727b6374eecf7
DevByWater <egaraudy@gmail.com>
2019-04-18 23:55:44 -0600

    update dependencies && add logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const logger = require('morgan')
 const dotenv = require('dotenv')
 const db_config = require('./config').db
 dotenv.config()
@@ -10,6 +11,7 @@ const app = express()
 
 // middleware
 app.use(bodyParser.json())
+app.use(logger('dev'))
 // api
 app.use('/api/v1', require('./routes/api/task'))
 // database setup
